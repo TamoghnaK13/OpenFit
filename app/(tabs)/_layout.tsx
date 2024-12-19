08fe2b019ad9115fad6c6f81b43c1971b2b273f3
Iain Broomell <slaprabbit805@gmail.com>
2024-12-19 13:28:54 -0500

    so I did a bunch of the tutorial and now know how to make a basic app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,20 +5,30 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: '#25292e',
+        tabBarActiveTintColor: '#ffd33d',
+        headerStyle: {
+          backgroundColor: '#25292e',
+        },
+        headerShadowVisible: false,
+        headerTintColor: '#fff',
+        tabBarStyle: {
+          backgroundColor: '#25292e',
+        },
       }}
     >
-      <Tabs.Screen name="index" options={{ 
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
-          ) }}
+      <Tabs.Screen name="index" options={{
+        title: 'Home',
+        tabBarIcon: ({ color, focused }) => (
+          <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+        )
+      }}
       />
-      <Tabs.Screen name="about" options={{ 
+      <Tabs.Screen name="about" options={{
         title: 'About',
         tabBarIcon: ({ color, focused }) => (
           <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
-        ) }} 
+        )
+      }}
       />
     </Tabs>
   )
